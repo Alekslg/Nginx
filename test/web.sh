9e40c9553442420aee74dd21695db3c746b3ef46
aleks <aleks@localhost.localdomain>
2018-12-26 23:33:04 +0300

    test 01

--- test/web.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir ./web/public/img -p
-mkdir ./web/public/css
-mkdir ./web/public/js
-mkdir ./web/uploads
-mkdir ./web/etc
--- web.sh
+++ web.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-pwd
+a='./web/public/img'
+b='./web/public/css'
+c='./web/public/js'
+d='./web/uploads'
+e='./web/etc'
+
+touch $a/.0.tmp
+touch $b/.0.tmp 
+touch $c/.0.tmp
+touch $d/.0.tmp
