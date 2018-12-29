2cb5396e3e7a32c37ba495523dad3c8e6dc75423
Aleks <aaa@sss.ew>
2018-12-29 16:09:10 +0300

    fsdf

--- in.sh
+++ in.sh
@@ -2,5 +2,5 @@
 echo "Start"
 rm /etc/nginx/nginx.conf
 # ln -s ~/web/etc/nginx.conf /etc/nginx/nginx.conf
-ln -s /home/aleks/web/etc/nginx.conf /etc/nginx/nginx.conf
+ln -s /home/aleks/web/etc/nginx.conf /etc/nginx/default.d/add_nginx.conf
 echo "Ready"
