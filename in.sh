7860f6bfdabc9f113cdf4b9ff6e591f27a58910e
aleks <aleks@localhost.localdomain>
2018-12-30 17:34:29 +0300

    good

--- in.sh
+++ in.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Start"
-rm /etc/nginx/nginx.conf
-# ln -s ~/web/etc/nginx.conf /etc/nginx/nginx.conf
-ln -s /home/aleks/web/etc/nginx.conf /etc/nginx/default.d/add_nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 echo "Ready"
