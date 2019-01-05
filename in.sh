3b86db026b62c073e4a69cf862e41f58188ec8a2
Aleks <you@example.com>
2019-01-06 00:59:49 +0300

    new2

--- in.sh
+++ in.sh
@@ -3,5 +3,5 @@ echo "Start"
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/nginx restart
-sudo service gunicorn restart
+gunicorn -c etc/hello.py hello:app
 echo "Ready"
