52cacede5b0756ba864e00eb60fb331eb541c7fb
Aleks <Aleks3ds@gmail.com>
2019-01-09 13:38:22 +0000

    django-01

--- etc/hello.py
+++ etc/hello.py
@@ -2,5 +2,6 @@ import multiprocessing
 mode = "wsgi"
 bind = "0.0.0.0:8080"
 # workers= "2"
-workers = ((multiprocessing.cpu_count() * 2) + 1)
+#workers = ((multiprocessing.cpu_count() * 2) + 1)
+workers = multiprocessing.cpu_count()
 timeout= "60"
